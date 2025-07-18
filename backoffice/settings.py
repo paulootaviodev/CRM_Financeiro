8243c72e8537a7a873a0d2746eace5710b85e195
Paulo Otávio <paulootavio343@outlook.com>
2025-07-18 18:42:36 -0300

    Remove the Django admin panel

--- backoffice/settings.py
+++ backoffice/settings.py
@@ -38,7 +38,6 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
